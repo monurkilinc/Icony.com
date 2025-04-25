53e10131fb6fd1db20367d45444f831c3fb90c0d
monurkilinc <monurkilinc@gmail.com>
2025-04-25 18:21:17 +0300

    ProductController olusturuldu. Prodcut CRUD indexleri olusturuldu AdminLayout olustuldu

---
+++ Icony.com/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using Icony.Core.Entities;
+using Icony.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Icony.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products
+                                        .Include(p => p.Category)
+                                        .ToListAsync();
+            return View(products);
+        }
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product=await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+            return View(product);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product= await _context.Products
+                                        .Include(p=>p.Category)
+                                        .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+                return(NotFound());
+
+            return View(product);
+        }
+        
+    }
+}
